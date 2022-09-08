ffddc672c210bf01044b76d83f1b3e8ead1b3938
Amab <miynaoje@outlook.com>
2022-09-08 05:22:42 -0700

    printing in c using uts ad printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include<stdio.h>
 
 /*
- * main -Entry point
- * Return: always (0)
+ * main - Entry point
+ * Return: Always 0 (Succes)
  */
 
-int main (void)
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-
-		return (0);
+	puts("\"Programming is like buildinga multilingual puzzle");
+	return (0);
 }
