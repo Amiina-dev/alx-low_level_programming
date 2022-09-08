02d4c054c6c50e136fbec893ed430ae4ec870080
Amab <miynaoje@outlook.com>
2022-09-08 05:08:37 -0700

    print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,45 +1,13 @@
-#include <stdio.h>                                                                                                              
+#include<stdio.h>
 
-                                                                                                                                
+/*
+ * main -Entry point
+ * Return: always (0)
+ */
 
-                                                                                                                                
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
 
-                                                                                                                                
-
-/**                                                                                                                             
-
- *                                                                                                                              
-
- *  *main - Entry point                                                                                                         
-
- *                                                                                                                              
-
- *   *Return: Always 0 (Success)                                                                                                
-
- *                                                                                                                              
-
- *    */                                                                                                                        
-
-                                                                                                                                
-
-                                                                                                                                
-
-                                                                                                                                
-
-int main(void)                                                                                                                  
-
-                                                                                                                                
-
-{                                                                                                                               
-
-                                                                                                                                
-
-                puts("\"Programming is like building a multilingual puzzle");                                                   
-
-                                                                                                                                
-
-                        return (0);                                                                                             
-
-                                                                                                                                
-
-}       
+		return (0);
+}
