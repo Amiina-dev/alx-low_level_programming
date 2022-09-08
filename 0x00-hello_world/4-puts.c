0f68f7fa93940a9517e568098f4d103ff073a30f
Amab <miynaoje@outlook.com>
2022-09-08 05:32:18 -0700

    print using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 
 /*
- * main - Entry point
- * Return: Always 0 (Succes)
+ *main - Entry point
+ *return: Always 0 (Success)
  */
 
 int main(void)
 {
-	puts("\"Programming is like buildinga multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
