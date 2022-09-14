48e1a3e0cce96c1d80a055a3cde3d1ff8385f3b1
Amab <miynaoje@outlook.com>
2022-09-13 18:45:20 -0700

    return abs value

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,10 +8,15 @@
 
 int print_last_digit(int n)
 {
+	int a;
+
 	if (n < 0)
-		n *= -1;
+		n = -n;
+		a = n % 10;
 
-	_putchar('0' + (n % 10));
+	if (a < 0)
+		a = -a;
+		_putchar(a + '0');
 
-	return (n % 10);
+	return (a);
 }
