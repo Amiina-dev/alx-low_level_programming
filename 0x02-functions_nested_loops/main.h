50d069fa370a9b187f85ebcc23265410a16ad634
Amab <miynaoje@outlook.com>
2022-09-13 17:47:04 -0700

    return abs value

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -34,13 +34,13 @@ int print_last_digit(int);
 
 void jack_bauer(void);
 
-void time_teble(void);
+void times_table(void);
 
 int add(int, int);
 
 void print_to_98(int n);
 
-void print_time_table(int n);
+void print_times_table(int n);
 
 
 
