67466149d4c8bd9cbc93e0687740a44042e85926
Amab <miynaoje@outlook.com>
2022-09-13 17:56:21 -0700

    return abs value

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *main - prints the first 50 fibonacci numbers
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int counter;
+	int count_to = 50;
+	long a = 1;
+	long b = 2;
+
+	for (counter = 1; counter <= (countto / 2); counter++)
+	{
+		printf("%li %li ", a, b);
+		a += b;
+		b += a;
+	}
+	if (countto % 2 == 1)
+		printf("%li", a);
+
+	printf("\n");
+
+	return (0);
+}
