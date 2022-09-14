3c8ac908262c967a32aac8a63cad5b69f76114ac
Amab <miynaoje@outlook.com>
2022-09-13 17:16:35 -0700

    return abs value

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *add - adds two integers
+ *@a: first integer arguement
+ *@b: second integer arguement
+ *Return: Sum
+ */
+
+int add(int a, int b)
+{
+	int c = a + b;
+	_putchar('\n');
+	return (c);
+}
