990cd54718299121885014c2ffb871a1e9b06ce8
Amab <miynaoje@outlook.com>
2022-09-15 13:27:34 -0700

    More nested loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,18 +2,16 @@
 
 /**
  * print_numbers - prints numbers 0-9
- * Return: Always 0 (Success)
+ * Return: void
  */
 
 void print_numbers(void)
 {
-	int c;
+	char c;
 
-	for (c = 0; c <= 9; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 		_putchar(c);
 	}
 	_putchar('\n');
-
-	return (0);
 }
