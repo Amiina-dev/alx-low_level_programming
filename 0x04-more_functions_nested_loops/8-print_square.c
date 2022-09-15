c887e67882232266a1430cd97204483298955b4c
Amab <miynaoje@outlook.com>
2022-09-15 14:04:12 -0700

    More nested loops

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square on the terminal
+ * @size: size of the square
+ */
+
+void print_square(int size)
+{
+	int row;
+	int column;
+
+	if (size > 0)
+	{
+		for (row = 1; row <= size; row++)
+		{
+			for (column = 1; column <= size; column++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else 
+		_putchar('\n');
+}
