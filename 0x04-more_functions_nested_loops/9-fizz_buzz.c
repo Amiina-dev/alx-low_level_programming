e202e4555e05896b7ef6eba03e9d7c1ffcbba79b
Amab <miynaoje@outlook.com>
2022-09-15 14:14:21 -0700

    More nested loops

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * main - print 1-100
+ * multiples of 3 with fizz
+ * multiples of 5 with buzz
+ * multiples of 3 and 5 with fizzbuzz
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0	&& i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		if (i < 100)
+			printf(" ");
+	}
+	printf("\n");
+
+	return (0);
+}
