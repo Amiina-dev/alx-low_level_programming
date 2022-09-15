05b2d8f49e23d2c0e299e162793dfe89453e1295
Amab <miynaoje@outlook.com>
2022-09-15 04:53:48 -0700

    Debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,7 +12,6 @@ int main(void)
 	printf("Infinite loop incoming: :(\n");
 
 	i = 0;
-
 	/**
 	 *while (i < 10)
 	 *{
@@ -20,7 +19,6 @@ int main(void)
 	 *
 	 *}
 	 */
-
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
