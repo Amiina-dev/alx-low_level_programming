50372b9d3a9f04f01bb1db9b7a39ae78423f05d5
Amab <miynaoje@outlook.com>
2022-09-19 04:01:16 -0700

    Pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swap two integers
+ * @a: first integer
+ * @b: second integer
+ */
+
+void swap_int(int *a, int *b)
+{
+	int d;
+
+	d = 0;
+	d = *a;
+	*a = *b;
+	*b = d;
+}
