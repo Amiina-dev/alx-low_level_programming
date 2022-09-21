9799edec3213bb41914f1b3440aeccba5ad3a231
Amab <miynaoje@outlook.com>
2022-09-21 16:09:28 -0700

    ointers

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcmp - comparestwo strings
+ * @s1: first string
+ * @s2: secong string
+ * Return: 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	while (*s1 == *s2 && *s1 != '\0')
+	{
+		s1++;
+		s2++;
+	}
+	if (s1 != s2)
+		result = *s1 - *s2;
+	return (result);
+}
