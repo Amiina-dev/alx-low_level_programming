22c591748ee21be9d530bb1a6828ada9c184e4d8
Amab <miynaoje@outlook.com>
2022-09-21 16:14:18 -0700

    Pointers

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse array elements
+ * @a: array
+ * @n: number of elements in the array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int tmp, beg = 0;
+	int end = n - 1;
+
+	while (beg < end)
+	{
+		tmp = *(a + beg);
+		*(a + beg) = *(a + end);
+		*(a + end) = tmp;
+		beg++, end--;
+	}
+}
