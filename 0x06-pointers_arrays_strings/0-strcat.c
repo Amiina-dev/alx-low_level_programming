49a13b4a0ae66ff7fdf5c30c8a5d76a18ee1436c
Amab <miynaoje@outlook.com>
2022-09-21 15:02:07 -0700

    Pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -17,11 +17,11 @@ char *_strcat(char *dest, char *src)
 
 	while (*(src + j))
 	{
-		*(dest+i) = *(src+j);
+		*(dest + i) = *(src + j);
 		i++;
 		j++;
 	}
-	*(dest+i) = '\0';
+	*(dest + i) = '\0';
 
 	return (dest);
 }
