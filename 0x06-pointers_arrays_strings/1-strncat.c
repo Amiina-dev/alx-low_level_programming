fb477c5ed0642b8a2a86f149edf8a263d1b71b31
Amab <miynaoje@outlook.com>
2022-09-21 15:32:30 -0700

    pointers

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncat - function that concatenates two strings
+ * @dest: string to be appended to 
+ * @src: string to append
+ * @n: append n number of characters
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	while (dest)
+		i++;
+
+	for (i = 0; i < n; i++)
+	{
+		while (*src)
+		{
+			*dest = *src;
+			dest++;
+			src++;
+		}
+		*dest = '\0';
+	}
+	return (dest);
+}
