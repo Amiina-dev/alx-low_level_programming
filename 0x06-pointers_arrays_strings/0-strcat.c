9fcd9a62f745e46bdc13c63338179b40ba9b77d9
Amab <miynaoje@outlook.com>
2022-09-21 15:00:08 -0700

    Pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,19 +9,19 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int i = 0;
+	int j = 0;
 
-	while (dest[i] != '\0')
+	while (*(dest + i))
 		i++;
 
-	while (src[j] != '\0')
+	while (*(src + j))
 	{
-		dest[i] = src[j];
+		*(dest+i) = *(src+j);
 		i++;
 		j++;
 	}
-	dest[i] = '\0';
+	*(dest+i) = '\0';
 
 	return (dest);
 }
