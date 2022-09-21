89df1386161228261dc85fac40a1958294d42be6
Amab <miynaoje@outlook.com>
2022-09-21 16:18:16 -0700

    Pointers

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper - converts a string to uppercase
+ * @s: string to be converted
+ * Return: caps string
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 'a' + 'A';
+	}
+	return (s);
+}
