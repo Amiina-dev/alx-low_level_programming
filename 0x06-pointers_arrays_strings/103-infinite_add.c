497e04e844cd470a40db7ad9ea1e250614664e66
Amab <miynaoje@outlook.com>
2022-09-21 17:49:46 -0700

    Pointers

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer
+ * @size_r: buffer size
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+}
