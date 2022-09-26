9a60071a893549d2dc62845d764f2214893b6367
Amab <miynaoje@outlook.com>
2022-09-26 11:43:52 -0700

    More pointers

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memry area
+ * @dest: destination
+ * @src: source
+ * @n: size
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		AV
+	}
+	return (dest);
+}
