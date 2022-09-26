43ee21a87f89adf3b3847393af474109364cac6a
Amab <miynaoje@outlook.com>
2022-09-26 13:37:53 -0700

    More pointers

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,20 +9,20 @@
 
 void print_diagsums(int *a, int size)
 {
-	int diagonalsumone = 0;
-	int diagonalsumtwo = 0;
+	int diagonal_sum_1 = 0;
+	int diagonal_sum_2 = 0;
 	int row, i;
 
 	for (row = 0; row < size; row++)
 	{
 		i = (row * size) + row;
-		diagonalsumone += a[i];
+		diagonal_sum_1 += a[i];
 	}
 
 	for (row = 1; row <= size; row++)
 	{
 		i = (row * size) - row;
-		diagonalsumtwo += a[i];
+		diagonal_sum_2 += a[i];
 	}
 
 	printf("%d, %d\n", diagonal_sum_1, diagonal_sum_2);
