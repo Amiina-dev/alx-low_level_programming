7ab2a10279b918ebf7af40f2cee836bf55e735ec
Amab <miynaoje@outlook.com>
2022-09-26 11:59:42 -0700

    More pointers

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,13 +9,17 @@
 
 char *_strchr(char *s, char c)
 {
-	int i = 0;
-
-	while (*(s + i) && *(s + i) != c)
-		i++;
-
-	if (*(s + i) == c)
-		return (s + i);
-	else
-		return (NULL);
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (*s == c)
+	{
+		return (s);
+	}
+	return (0);
 }
