f4282b3f4c5d3c4b588bf16bb55422d1a17a8590
Amab <miynaoje@outlook.com>
2022-09-26 13:30:01 -0700

    More pointers

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagsums - print sums of diagonals in matrix
+ * @a: matrix
+ * @size: size of matrix
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int diagonalsumone = 0;
+	int diagonalsumtwo = 0;
+	int row,i;
+
+	for (row = 0; row < size; row++)
+	{
+		i = (row * size) + row;
+		diagonalsumone += a[i];
+	}
+
+	for (row = 1; row <= size; row++)
+	{
+		i = (row * size) - row;
+		diagonalsumtwo += a[i];
+	}
+
+	printf("%d, %d\n", diagonal_sum_1, diagonal_sum_2);
+}
