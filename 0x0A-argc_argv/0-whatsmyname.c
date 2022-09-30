fb349f1becc4e32a4061ef7372bacb44195cbca9
Amab <miynaoje@outlook.com>
2022-09-30 13:48:20 -0700

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - write a program that prints its name followed by a new line. If the file is renamed, it will print the new name
- * without compiling again. You should remove the path before the name of the program.
- *
+ * main - write a program that prints its name followed by a new line.
+ * If the file is renamed, it will print the new name
+ * without compiling again.
  * @argc: arguement count
  * @argv: arguement vector
  * Return: 0
  */
 
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
-
 	return (0);
 }
