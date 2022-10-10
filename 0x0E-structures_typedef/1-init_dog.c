03c661298acfc025273189c32e3d158abbd95aea
Amab <miynaoje@outlook.com>
2022-10-10 16:07:58 -0700

    corrections

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -6,7 +6,7 @@
  * @d: A dog structure.
  * @name: name of the dog
  * @age: age of the dog
- * @owner: owner of the dog 
+ * @owner: owner of the dog
  * Return: Nothing
  */
 
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,7 +8,7 @@
  * @owner: third member
  */
 
-struct dog 
+struct dog
 {
 	char *name;
 	float age;
