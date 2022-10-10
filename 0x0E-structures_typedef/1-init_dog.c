f70d6c386984f4909d282381cea1f92e8d4314d2
Amab <miynaoje@outlook.com>
2022-10-10 15:56:11 -0700

    corrections

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,6 +7,7 @@
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog 
+ * Return: Nothing
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,12 +2,6 @@
 #define _HEADER_
 
 /**
- * dog_t - Typedef for struct dog
- */
-
-typedef struct dog dog_t;
-
-/**
  * struct dog - structure for dogs
  * @name: first member
  * @age: second member
@@ -20,6 +14,11 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - Typedef for dog structure
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
