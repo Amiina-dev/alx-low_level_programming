301a5997b71ece38f5bac7f9173b810b67181062
Amab <miynaoje@outlook.com>
2022-10-12 03:33:59 -0700

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "functions_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - prints a persons name
+ * @name: name to be printed
+ * @f: pointer to function
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL)
+		f(name);
+}
