d8acb792e76f8b3435ec2d3238d042857c1d514a
Amab <miynaoje@outlook.com>
2022-10-13 02:36:45 -0700

    Task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * print_all - prints everything
+ * @format: list of all arguements passed to the function 
+ * Return: void
+ */
+
+void print_all(const char * const format, ...)
+{
+	unsigned int i;
+	va_list args;
+	char *s, *seperator;
+
+	va_start(ags, format);
+
+	seperator = "";
+
+	i = 0;
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%s%c", seperator, va_arg(args, int));
+				break;
+			case 'i':
+				printf("%s%d", seperator, va_arg(args, int));
+				break;
+			case 'f':
+				printf("%s%f", seperator, va_arg(args, double));
+				break;
+			case 's':
+				s = va_arg(args, char *);
+				if (s ==NULL)
+					s = "(nil)";
+				printf("%s%s", seperator, s);
+				break;
+			default:
+				i++;
+				continue;
+		}
+		seperator = ", ";
+		i++;
+	}
+
+	printf("\n");
+	va_end(args);
+}
