03f3ae917b5a42a6cdf7ac35a53d709953f5e256
Amab <miynaoje@outlook.com>
2022-10-31 16:13:51 -0700

    I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,15 +12,19 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd;
 	int i, y;
 	char *buf;
+
 	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY);
+
 	if (fd < 0)
 		return (0);
 	buf = malloc(sizeof(char) * letters);
+
 	if (!buf)
 		return (0);
 	i = read(fd, buf, letters);
+
 	if (i < 0)
 	{
 		free(buf);
@@ -29,12 +33,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	buf[i] = '\0';
 	close(fd);
 	y = write(STDOUT_FILENO, buf, i);
+
 	if (y < 0)
 	{
 		free(buf);
 		return (0);
 	}
 	free(buf);
-
 	return (y);
 }
