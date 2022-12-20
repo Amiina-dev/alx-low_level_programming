8fa9e5497d131a54977f046e85a4a6b3484ca823
Amab <miynaoje@outlook.com>
2022-12-20 13:43:41 -0800

    ynamic Libraries

---
+++ 0x18-dynamic_libraries/3-op_functions.c
@@ -0,0 +1,43 @@
+/**
+ * add - add
+ * @a: int
+ * @b: int
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract
+ * @a: integer
+ * @b: integer
+ * Return: result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * divide - divide
+ * @a: int
+ * @b: int
+ * Return: result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - mod
+ * @a: int
+ * @b: int
+ * Return: result
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
